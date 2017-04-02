298dcfbb5f3877300acadf9d96ea3c730fa75ed3
rahsnh <rahsnh@gmail.com>
2017-04-02 23:37:19 +0530

    Add files via upload

---
+++ sports/index.php
@@ -0,0 +1,190 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+	
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"> 
+		<title>Advaita 2015-16 | IIIT Bhubaneswar.</title>
+<link rel="apple-touch-icon" sizes="57x57" href="../website/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../website/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../website/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../website/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../website/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../website/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../website/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../website/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../website/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../website/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../website/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../website/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../website/icon/favicon-16x16.png">
+<link rel="manifest" href="website/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="website/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="../css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="../css/item.css" />
+		<link rel="stylesheet" type="text/css" href="css/sports.css" />
+		<link rel="stylesheet" type="text/css" media="only screen and (min-width:300px) and (max-width:960px)"  href="../css/screen_layout_medium.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+		<script src="../js/modernizr.custom.js"></script>
+		<script src="../js/pace.js"></script>
+     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700&subset=latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin-ext' rel='stylesheet' type='text/css'>
+    
+    <link rel="stylesheet" href="../css/style.css"> <!-- Resource style -->
+	</head>
+	<body>
+		    			
+			<div class="bg-image">
+			<div class="spacing">
+			<div class="container">
+			<div class="content">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon-menu"></a>
+					<span class="gn-icon-menu-2"></span>
+					<span class="gn-icon-menu-3"></span>
+   					 <nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+                <li><a href="../tech" class="gn-icon gn-icon-download">TECHEON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../cult" class="gn-icon gn-icon-cog">ISLE OF EUPHORIA<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../literary" class="gn-icon gn-icon-help">ISLE OF ERUDITE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../ringoffire"class="gn-icon gn-icon-archive">RING OF FIRE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="#" class="gn-icon gn-icon-article">ISLE OF ATHLON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../art" class="gn-icon gn-icon-photoshop">SHUTTER ISLAND<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../workshop" class="gn-icon gn-icon-search">WORKSHOPS<img class="arrow" src="../img/arrow.png"/></a></li>
+              </ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="../"><img src="../Photos/logo.png" style="height:60px;"/></a></li>
+			</ul>
+			<header>
+				<h1>ISLE OF ATHLON</h1>	
+			</header> 
+		</div>
+		<div class="island-head">
+        <p>And so it all began a billion years ago....</p>
+            <p>It was the day of the Athlympics, the Ring’s biggest sporting event. 
+The humongous arena boasted of huge swimming pools, various courts, wrestling rings, skating rinks, 
+and athletic tracks, besides housing huge restaurants and hotels for participants and  spectators alike. 
+It was a mini planet in itself.  
+Nobody expected the occurrence of a huge explosion, which devastated the large stadium 
+and separated the whole region from the Ring of Fire.</p>
+    </div>
+    <section class="cd-section">
+    <div class="cd-modal-action">
+      <a href="#" class="btn" data-type="modal-trigger">Continue Reading</a>
+      <span class="cd-modal-bg"></span>
+    </div> <!-- cd-modal-action -->
+
+    <div class="cd-modal">
+      <div class="cd-modal-content">
+        <p>The Great Rift is over, the old Ring of Fire lies in ruins, 
+the entire cosmos smattered with its debris from the ultimate catastrophe.</p>
+
+        <p>And so it all began a billion years ago....</p>
+
+        <p>It was the day of the Athlympics, the Ring’s biggest sporting event. 
+The humongous arena boasted of huge swimming pools, various courts, wrestling rings, skating rinks, and athletic tracks, 
+besides housing huge restaurants and hotels for participants and  spectators alike. It was a mini planet in itself.  </p>
+
+                <p>Nobody expected the occurrence of a huge explosion, which devastated the large stadium 
+and separated the whole region from the Ring of Fire. The survivors were mostly sportsmen and a few spectators. 
+When they came back to their senses, they found that they were floating in dark space, surrounded with asteroids, 
+comets, meteoroids ... and weird aliens ! The first few days went without much work, since food and water was available 
+but slowly the supplies reached the verge of depletion. Now their survival depended on only one option -  
+to hunt down the diabolical beings floating into the remnants of their devastated dimension. But who would hunt them down?</p>
+                <p>The shot-put and javelin throwers crafted a plan – the beasts shall be attracted and injured with the shot-put balls
+ and later hunted down with the javelins; it worked. Then the chess grandmasters started synthing water from the hydrogen 
+and the oxygen tanks in the debris. It took them many years, but life was finally restored .... to a new Athlon. </p>
+<p>Present Time.</p>
+                <p>It was time for the Athlympics again. Just as the Athlon Torch was lit to mark the beginning of the event, 
+the sun took on a green hue. The dreaded tell-tale pirates, sun-raiders of the Apostle Galaxy, the Scavengers had attacked ! 
+The Athlonites had no idea that they would witness an overwhelming Scavenger blitzkrieg !
+The brave sportsmen rushed the women and children to safety, and all protective measures and defenses were adopted. 
+But they knew they were no match for the mighty Scavengers.</p>
+                <p>And suddenly an unthinkable miracle happened - the Scavengers started retreating to their galaxy! 
+The Athlonites started celebrating, only to later know the real reason of the miracle - the heroics of the mighty Techeon 
+and Shutter Island. They decided to transport the Athlympics to the Ring of Fire where the other islands too would meet 
+after a long time. With the Athlympics, ADVAITA would become a never-before-seen spectacle in the history of the Ring of Fire.
+</p>
+      </div> <!-- cd-modal-content -->
+    </div> <!-- cd-modal -->
+
+
+		<a href="#" class="cd-modal-close">Close</a>
+	   </section>
+		
+             <div class="row">
+             <div class="col-md-12 col-md-offset-2">
+              <div class="col-md-4">
+                <figure class="opaque1 box-display border">
+                  <svg width="100%" height="100%">
+                   <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                   </g>
+                  </svg>
+                  <figcaption>
+                    <h3>FUTSAL</h3>
+                    <p class="description">A game much like football which is played over a smaller pitch and with
+a slightly different version of rules</p>
+
+                    <p style="text-align: center"><a href="futsal">Read More</a></p>
+                  </figcaption>
+                </figure>
+             </div>
+             <div class="col-md-4">
+                <figure class="opaque2 box-display border">
+                <svg width="100%" height="100%">
+                  <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                  </g>
+                 </svg>
+                  <figcaption>
+                    <h3>SUPER SIX</h3>
+                    <p class="description">Cricket is the most popular game in India.
+Teams from different colleges participate to win
+this unique format of the game</p>
+
+                    <p style="text-align: center"><a href="super-six">Read More</a></p>
+                  </figcaption>
+                </figure>
+             </div>
+            </div>
+         </div>
+           <br/><br />
+	    </div><!-- /container -->
+  </div>
+  </div>
+  <div style="background:#B0967B; width:100%; height:5px;"></div>
+  <?php include("../footer.html"); ?>
+
+        
+
+		<script src="../js/classie.js"></script>
+		<script src="../js/gnmenu.js"></script>
+		<script src="../js/velocity.min.js"></script>
+        <script src="../js/main.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+   	</body>
+</html>

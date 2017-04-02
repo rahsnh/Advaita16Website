a8f81f2139717e816c2e0ce91f4c49bd8692962a
rahsnh <rahsnh@gmail.com>
2017-04-02 23:28:24 +0530

    Add files via upload

---
+++ ringoffire/index.php
@@ -0,0 +1,211 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"> 
+		<title>Advaita 2015-16 | IIIT Bhubaneswar.</title>
+<link rel="apple-touch-icon" sizes="57x57" href="../website/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../website/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../website/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../website/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../website/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../website/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../website/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../website/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../website/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../website/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../website/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../website/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../website/icon/favicon-16x16.png">
+<link rel="manifest" href="website/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="website/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="../css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="../css/item.css" />
+		<link rel="stylesheet" type="text/css" href="css/ringoffire.css" />
+		<link rel="stylesheet" type="text/css" media="only screen and (min-width:300px) and (max-width:960px)"  href="../css/screen_layout_medium.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+		<script src="../js/modernizr.custom.js"></script>
+		<script src="../js/pace.js"></script>
+     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700&subset=latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin-ext' rel='stylesheet' type='text/css'>
+                        
+	<link rel="stylesheet" href="../css/style.css"> <!-- Resource style -->
+	</head>
+	<body>
+						
+			<div class="bg-image">
+			<div class="spacing">
+			<div class="container">
+			<div class="content">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon-menu"></a>
+					<span class="gn-icon-menu-2"></span>
+					<span class="gn-icon-menu-3"></span>
+   					 <nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+                <li><a href="../tech" class="gn-icon gn-icon-download">TECHEON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../cult" class="gn-icon gn-icon-cog">ISLE OF EUPHORIA<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../literary" class="gn-icon gn-icon-help">ISLE OF ERUDITE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="#"class="gn-icon gn-icon-archive">RING OF FIRE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../sports" class="gn-icon gn-icon-article">ISLE OF ATHLON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../art" class="gn-icon gn-icon-photoshop">SHUTTER ISLAND<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../workshop" class="gn-icon gn-icon-search">WORKSHOPS<img class="arrow" src="../img/arrow.png"/></a></li>
+              </ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="../"><img src="../Photos/logo.png" style="height:60px;"/></a></li>
+			</ul>
+			<header>
+				<h1>RING OF FIRE</h1>	
+			</header> 
+		</div>
+		<div class="island-head">
+        <p>Billions of years ago, in a galaxy light years away, there existed 
+the Ring of Fire – a group of massive islands, home to the most intelligent life forms of the universe. 
+Some attribute its existence to the supernatural, claiming it to be a miracle. It is difficult to even 
+comprehend how a chunk of land orbiting near a star could herald the most advanced species 
+of the universe. The inhabitants had created a utopian civilization where people excelled in all fields - 
+science, sports, literature - name it and you will find a Ring islander seated at the pinnacle.</p>
+    </div>
+    <section class="cd-section">
+    <div class="cd-modal-action">
+      <a href="#0" class="btn" data-type="modal-trigger">Continue Reading</a>
+      <span class="cd-modal-bg"></span>
+    </div> <!-- cd-modal-action -->
+
+    <div class="cd-modal">
+      <div class="cd-modal-content">
+        <p>Billions of years ago, in a galaxy light years away, 
+there existed the Ring of Fire – a group of massive islands, home to the most intelligent life forms 
+of the universe. Some attribute its existence to the supernatural, claiming it to be a miracle. It is difficult 
+to even comprehend how a chunk of land orbiting near a star could herald the most advanced species 
+of the universe. The inhabitants had created a utopian civilization where people excelled in all fields - 
+science, sports, literature - name it and you will find a Ring islander seated at the pinnacle.</p>
+
+        <p>The Ring basically consisted of huge concentric 
+land masses separated by moats and linked by canals flowing with super-dense space. 
+At the centre of the Ring stood a giant dormant volcano. Each of the six landmasses of the Ring 
+was called an Isle. People from each Isle excelled in one of many vocations - 
+The Techiodes from Techeon Isle were the pioneers of science and technology; Athlon trained 
+star sportsmen; Shutter Island was home to the Shutterbugs, the most creative people of the Ring 
+who were also master film-makers and photographers; Euphoria enriched minds with artistes and 
+exponents of music and dance, while Erudite was the land of genesis of great scholars of literature 
+and extempore. Besides, there were the cheerful Merrymakers from Funola, whose main aim in life 
+was to be happy and make merry.</p>
+
+        <p>Everything was going good till an energy crisis hit the Ring. 
+A civilization as big and advanced as the Ring requires a lot of energy. 
+The Islanders had every technology of energy production perfected, but with great prosperity comes great energy demands. 
+They went on innovating and engineering new ways to harness energy 
+from various sources. But they knew that they were going to hit rock bottom.</p>
+
+                <p>It was high time when they recognized the mounting energy signatures 
+from the dormant volcano. They very well understood the risk of encountering 
+a deadly volcanic explosion. But looking at the pressures of the rising energy crises, 
+the Isles decided to have a go at it. They sent their best miners and engineers to work. 
+But Murphy's Law came into action again. The dormant volcano erupted, causing 
+massive earthquakes and shockwaves that flung each Isle far away. 
+Millions of lives were lost. Whole landscapes were wiped out. In mere seconds, 
+every accomplishment of theirs was reduced to rubble.</p>
+                <p>The Isles blamed each other for what was termed as The Great Rift. 
+They decided to never meet, and all lines of communication were severed. 
+The Ring of Fire was now just the dormant volcano and the surrounding landmass, 
+formed from the solidified plasma erupted from the volcanic cone. 
+The Isles were now six distant, disconnected entities floating in empty space, each island considering the others as inexistent.</p>
+<p>As time passed, each island became the master in their 
+respective vocations. However, the idea of resuming 
+inter-island communication was limited to books, debates 
+and documentaries, even a billion years after the Great Rift.</p>
+                <p>Everything changed when the sun turned green....</p>
+                
+      </div> <!-- cd-modal-content -->
+    </div> <!-- cd-modal -->
+
+		<a href="#0" class="cd-modal-close">Close</a>
+	   </section>
+
+		
+             <div class="row">
+             <div class="col-md-12 col-md-offset-2">
+              <div class="col-md-4">
+                <figure class="opaque1 box-display border">
+                  <svg width="100%" height="100%">
+                   <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                   </g>
+
+                  </svg>
+                  <figcaption>
+                    <h3>PRONITE</h3>
+                    <p class="description">The one who sets the stage ablaze with his signature voice, Raghu Dixit.</p>
+
+                    <p style="text-align: center"><a href="raghu-dixit">Read More</a></p>
+                  </figcaption>
+                </figure>
+             </div>
+             <div class="col-md-4">
+                <figure class="opaque2 box-display border">
+                <svg width="100%" height="100%">
+                  <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                  </g>
+                  
+                 </svg>
+                  <figcaption>
+                    <h3>TECHNOVA</h3>
+                    <p class="description">Oportunity to each and every geek to flaunt their technical talent and a platform for the tech enthusiasts to display their technical skills and innovative ideas.</p>
+
+                    <p style="text-align: center"><a href="../tech/technova/">Read More</a></p>
+                  </figcaption>
+                </figure>
+             </div>
+             </div>
+         </div>
+           <br/><br/>
+	    </div><!-- /container -->
+  </div>
+  </div>
+  <div style="background:#232323; width:100%; height:5px;"></div>
+  <?php include("../footer.html"); ?>
+
+        
+
+		<script src="../js/classie.js"></script>
+		<script src="../js/gnmenu.js"></script>
+		<script src="../js/velocity.min.js"></script>
+        <script src="../js/main.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72394454-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	</body>
+</html>

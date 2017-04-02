878cb0fc773a287f0adcbad9d5a0b9f102e1def5
rahsnh <rahsnh@gmail.com>
2017-04-02 23:16:47 +0530

    Add files via upload

---
+++ literary/index.php
@@ -0,0 +1,219 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"> 
+		<title>Advaita 2015-16 | IIIT Bhubaneswar.</title>
+<link rel="apple-touch-icon" sizes="57x57" href="../website/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../website/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../website/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../website/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../website/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../website/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../website/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../website/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../website/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../website/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../website/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../website/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../website/icon/favicon-16x16.png">
+<link rel="manifest" href="website/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="website/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="../css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="../css/item.css" />
+		<link rel="stylesheet" type="text/css" href="css/literary.css" />
+		<link rel="stylesheet" type="text/css" media="only screen and (min-width:300px) and (max-width:960px)"  href="../css/screen_layout_medium.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+		<script src="../js/modernizr.custom.js"></script>
+		<script src="../js/pace.js"></script>
+
+     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700&subset=latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin-ext' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="../css/style.css"> <!-- Resource style -->
+	</head>
+	<body>
+		    
+			<div class="bg-image">
+			<div class="spacing">
+			<div class="container">
+			<div class="content">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon-menu"></a>
+					<span class="gn-icon-menu-2"></span>
+					<span class="gn-icon-menu-3"></span>
+   					 <nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+                <li><a href="../tech" class="gn-icon gn-icon-download">TECHEON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../cult" class="gn-icon gn-icon-cog">ISLE OF EUPHORIA<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="#" class="gn-icon gn-icon-help">ISLE OF ERUDITE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../ringoffire"class="gn-icon gn-icon-archive">RING OF FIRE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../sports" class="gn-icon gn-icon-article">ISLE OF ATHLON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../art" class="gn-icon gn-icon-photoshop">SHUTTER ISLAND<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../workshop" class="gn-icon gn-icon-search">WORKSHOPS<img class="arrow" src="../img/arrow.png"/></a></li>
+              </ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="../"><img src="../Photos/logo.png" style="height:60px;"/></a></li>
+			</ul>
+			<header>
+				<h1>ISLE OF ERUDITE</h1>	
+			</header> 
+		</div>
+		<div class="island-head">
+        <p>As the first rays of the sun touched the Erudome tower, Princess Ogma sauntered into her beautiful flowered mezzanine, 
+and laid her eyes upon the pale blue sky, for what seemed to be the beginning of just another day. Bestowed with boundless intelligence, wisdom 
+and beauty from birth, she was blessed to the royal Thoth bloodline of the Isle of Erudite, whose pioneers had succumbed to the burns 
+of the volcanic eruption that occurred two billion years ago.</p>
+    </div>
+    <section class="cd-section">
+    <div class="cd-modal-action">
+      <a href="#0" class="btn" data-type="modal-trigger">Continue Reading</a>
+      <span class="cd-modal-bg"></span>
+    </div> <!-- cd-modal-action -->
+
+    <div class="cd-modal">
+      <div class="cd-modal-content">
+        <p>As the first rays of the sun touched the Erudome tower, Princess Ogma sauntered into her beautiful flowered mezzanine, 
+and laid her eyes upon the pale blue sky, for what seemed to be the beginning of just another day. Bestowed with boundless intelligence, wisdom 
+and beauty from birth, she was blessed to the royal Thoth bloodline of the Isle of Erudite, whose pioneers had succumbed to the burns 
+of the volcanic eruption that occurred two billion years ago.</p>
+
+        <p>“Good Morning Princess! With the bees humming like a golden lyre, the sea breeze wafting pink petals higher, 
+the tower sparkles from its highest spire … oh look! Just arrived – a scroll from the Ring of Fire!” said Anahita, Princess Ogma’s maid. 
+And the Princess knew it’s the carnival.  A century has gone by, and it was time for the whole Realm to meet again at Advaita, the Grand Carnival, 
+a symbol of solidarity of the seven islands.</p>
+
+        <p>“Anahita, send my greetings to the four angels! Tell them, to await my visit! 
+Send for Odin, Oya and the Twins! For the moment of The Great Discourse has arrived!”</p>
+
+                <p>Every year the island witnesses what in itself is the manifestation of profound knowledge. 
+Great scholars, intelligent men, women, flora and fauna (yes, you read it right) 
+travel across the ten lands and twenty seas to attend the famous debate which no court, parliament, council, 
+senate, assembly, cabinet or ministry ever beholds - The Great Discourse. “Sister!  I have just the thing that you yearn - 
+the exact statistics of our present economic condition,” said Odin, Ogma’s younger brother, 
+the most accurate economist and mathematician ever known.</p>
+                <p>“It’s the Great Discourse I wish to talk to you about. Invite all our learned friends and colleagues! Set the stage right! 
+We must discuss and parley before we set foot on the Ring of Fire.” 
+“The Ring of Fire? You mean it’s time?” asked Odin, unable to hide the excitement in his voice.</p>
+                <p>Ogma sighed, “Yes, it IS, my dear. And before we depart we have to know it all.” 
+Odin understood that The Great Discourse of this year would be like none before. They had to discuss the welfare of the whole Realm this time before meeting the four angels. 
+Anyone who visited the Ring of Fire would know about the Quadrangelion - the four divine angels 
+that stand guard over the sacred fire there, each of them belonging to a royal bloodline of the Isle of Erudite. 
+Their inscrutable presence in the Ring of Fire was known to none but the Thoth dynasty.</p>
+<p>“Good Morning Princess!” said the Twins in unison. Caton and Chiron were undoubtedly 
+the most adored brothers on the island, the former born with an infinite information registry in his brain, the latter, a genius, possessing an innate ability of mind-reading.</p>
+                <p>“Hello cousins!” greeted Odin. “Our Sister here demands your attention for-” 
+“The Great Discourse!” said Chiron, completing Odin’s sentence. “Go on.  Why don’t you tell what’s more to it?” 
+said Odin quietly, his fury enshrouded. Princess Ogma suppressed her laugh. “Brothers! We have no time to lose! 
+Caton and Chiron, I want you both to arrange the whole debate. We have to visit the Ring of Fire this year, 
+so arrange the debate accordingly, from start to finish. Decide the subject, the whole procedure, and the judges. 
+Make arrangements for parchments and ink too. I provide you both a week to settle it all. Now, off I go to see what’s taking my little sister so long!”</p>
+<p>Oya, an enfant terrible, the one with velvet gloves delicately concealing iron fists beneath, was young, 
+yet her voracious reading habits had given her abundant knowledge in almost every subject present on the seven lands.</p>
+                <p>“Oya ! I’ve been looking out for you.” said Ogma. Oya exclaimed, “Sister! I had to finish this book! 
+This talks about the great sailors of our island, their navigation techniques, lifestyles and-” 
+“We don’t have time, Oya! We will be having The Great Discourse next week, and then visit the Ring of Fire! 
+I need you to help Odin with the arrangements. Now, will you please leave that book and get along?”</p>
+                <p>A week later, the Isle of Erudite welcomed hundreds of scholars, men and women, flora and fauna 
+to witness the event everyone was desperate to behold. As the tower bells rang aloud, the doors to the Erudome court opened.
+ The stage was set. The judges were- Brighid Thoth, the oldest member of the Thoth family; Lugh Neith, the prime minister 
+of the Erudome court; and the lovely princess Ogma. The first round of the event was already over two days before, where 
+the participants had to submit a written document on any topic of their convenience. The three best writings were selected 
+by Odin and Oya, and the corresponding entrants were then cordially invited to the Erudome court to gear up for the great debate. 
+The terrific trio comprised - Sir Mergen, the oldest monk in the Island; Sia, the housemaid of the Neith family; and Nidaba, 
+a famous lioness of the Eru Woods. Anahita graced the dais as host. Caton and Chiron had indeed done their work well.</p>
+                <p>“Welcome folks to the Erudome towers! We shall now travel on the tides of wit, words, and magic! 
+This evening will bring us the best orator of our three grand finalists! So let’s welcome them with a grand applause! 
+And … here comes the theme –“Anahita waved her voice and fingers, and the words ‘SHOULD ERUDITE JOIN ADVAITA?’ 
+glowing in bright gold sparkled in mid-air. The crowd hooted and thunder of claps followed 
+as the three matched towards the podium.</p>
+                <p>The next few hours were spellbinding. Sir Mergen and Sia had decided to go against the motion, 
+and came up with valid points like channeling the would-be expenditure rather towards the welfare of the people 
+of all islands instead of towards ‘not-so-important pursuits’. Nidaba decided to opt for the motion, her presence itself 
+intimidating all around her, her words more so. She beautifully explained as to why it is necessary for Erudite to join Advaita. 
+“For the holistic development of the whole realm it must be made mandatory to meet every year and collaborate and discuss 
+business proposals, development strategies, the realm's trade prospects and much more. The six islands must maintain harmony 
+and proper contact with each other for all the Islands to flourish. The recent Scavenger Attack has brought forth the bravado and willingness 
+to cooperate of the islands, and Advaita is its vantage point - the symbol of solidarity.”</p>
+                <p>Finally, after tense hours of crucial discussion, the judges, impressed by Nidaba’s jingoism for the whole realm, 
+voted her the winner of The Great Discourse-2016. Ogma presented the lioness with a beautiful tiara and made her 
+the Fauna Queen of the Eru Woods.</p>
+                <p>Later, when the princess and her subjects were packing bags for the carnival, she decided to press the matter 
+of arranging the carnival every year in front of all the other Islands. With this kind motive in her mind, she set forth for the grand carnival - Advaita.</p>
+      </div> <!-- cd-modal-content -->
+    </div> <!-- cd-modal -->
+
+
+		<a href="#0" class="cd-modal-close">Close</a>
+	   </section>
+             <div class="row">
+              <div class="col-md-4  col-md-offset-2">
+                <figure class="opaque1 box-display border">
+                  <svg width="100%" height="100%">
+                   <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                   </g>
+                  </svg>
+                  <figcaption>
+                    <h3>ACROPOLIS</h3>
+                    <p class="description">Coming Soon.Please visit our website for any updates</p>
+
+                    <!--<p style="text-align: center"><a href="#">Read More</a></p>-->
+                  </figcaption>
+                </figure>
+             </div>
+             <div class="col-md-4">
+                <figure class="opaque2 box-display border">
+                <svg width="100%" height="100%">
+                  <g id="large-devices">
+                    <line class="top" x1="0" y1="0" x2="300%" y2="0"/>
+                    <line class="left" x1="0" y1="210" x2="0" y2="-230"/>
+                    <line class="bottom" x1="0" y1="210" x2="300%" y2="210"/>
+                    <line class="right" x1="99.75%" y1="0" x2="99.75%" y2="400"/>
+                  </g>
+                 </svg>
+                  <figcaption>
+                    <h3>SPELL BEE</h3>
+                    <p class="description">Coming Soon.Please visit our website for any updates</p>
+
+                    <!--<p style="text-align: center"><a href="#">Read More</a></p>-->
+                  </figcaption>
+                </figure>
+             </div>    
+         </div>
+           <br/><br/>
+	    </div><!-- /container -->
+  </div>
+  </div>
+  <div style="background:#B0967B; width:100%; height:5px;"></div>
+  <?php include("../footer.html"); ?>
+
+        
+
+		<script src="../js/classie.js"></script>
+		<script src="../js/gnmenu.js"></script>
+		<script src="../js/velocity.min.js"></script>
+        <script src="../js/main.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  	</body>
+</html>

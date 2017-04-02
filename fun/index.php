96be27eef41e3c2688f5a229a6a2f8ceb8b05292
rahsnh <rahsnh@gmail.com>
2017-04-02 23:01:39 +0530

    Add files via upload

---
+++ fun/index.php
@@ -0,0 +1,134 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0"> 
+		<title>Advaita 2015-16 | IIIT Bhubaneswar.</title>
+<link rel="apple-touch-icon" sizes="57x57" href="../website/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../website/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../website/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../website/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../website/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../website/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../website/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../website/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../website/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../website/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../website/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../website/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../website/icon/favicon-16x16.png">
+<link rel="manifest" href="website/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="website/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+		<link rel="stylesheet" type="text/css" href="../css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="../css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<link rel="stylesheet" type="text/css" href="../css/item.css" />
+		<link rel="stylesheet" type="text/css" href="css/fun.css" />
+		<link rel="stylesheet" type="text/css" media="only screen and (min-width:300px) and (max-width:960px)"  href="../css/screen_layout_medium.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+		<script src="../js/modernizr.custom.js"></script>
+		<script src="../js/pace.js"></script>
+
+     <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:700&subset=latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto&subset=latin-ext' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="../css/style.css"> <!-- Resource style -->
+	</head>
+	<body>
+		    
+			<div class="bg-image">
+			<div class="spacing">
+			<div class="container">
+			<div class="content">
+			<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon-menu"></a>
+					<span class="gn-icon-menu-2"></span>
+					<span class="gn-icon-menu-3"></span>
+   					 <nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+                <li><a href="../tech" class="gn-icon gn-icon-download">TECHEON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../cult" class="gn-icon gn-icon-cog">ISLE OF EUPHORIA<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="#" class="gn-icon gn-icon-help">ISLE OF ERUDITE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../ringoffire"class="gn-icon gn-icon-archive">RING OF FIRE<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../sports" class="gn-icon gn-icon-article">ISLE OF ATHLON<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../art" class="gn-icon gn-icon-photoshop">SHUTTER ISLAND<img class="arrow" src="../img/arrow.png"/></a></li>
+                <li><a href="../workshop" class="gn-icon gn-icon-search">WORKSHOPS<img class="arrow" src="../img/arrow.png"/></a></li>
+              </ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="../"><img src="../Photos/logo.png" style="height:60px;"/></a></li>
+			</ul>
+			<header>
+				<h1>FUNOLA</h1>	
+			</header> 
+		</div>
+		<div class="island-head">
+		    <p>The residents of this island are not gifted with any magical powers. They don’t have any governing body. They are free thinkers and love being 
+        independent and thus declared themselves as their own leaders. </p>
+		</div>
+		<section class="cd-section">
+		<div class="cd-modal-action">
+			<a href="#" class="btn" data-type="modal-trigger">Continue Reading</a>
+			<span class="cd-modal-bg"></span>  
+		</div> <!-- cd-modal-action -->
+
+		<div class="cd-modal">
+			<div class="cd-modal-content">
+				<p>The residents of this island are not gifted with any magical powers. They don’t have any governing body. They are free thinkers and love being 
+        independent and thus declared themselves as their own leaders. </p>
+
+				<p>They believe that “Smile is the best way to face every problem, to hide every pain and to crush every fear.” Many of them lost their near and 
+        dear ones in the catastrophe caused by the Great Rift, but they all stood determined and spirited. They put constant efforts to bring smiles on 
+        each other’s face and live a cheerful and satisfied life. They kept themselves occupied with playing games, performing acts and celebrating. Years 
+        passed and one fine morning appeared the guardian angel of peace and appreciated them for the unity and harmony they maintained among themselves and 
+        as a regard with great respect christened their island as “Fun-o-Land”. The residents of this island became famous and are admired for boosting high 
+        spirits within individuals by organising fun events and sharing glee.</p>
+
+				<p>Little did the merrymakers know that their troubles had not ended. They were busy in preparation of the annual carnival when the sun turned green. 
+        The smiles faded.The music stopped.It took seconds for the gloom to take over.They knew something terrible was going to happen. Was one Great Rift not 
+        enough? The residents started preparing for doomsday.Everyone  returned to their families to say their goodbyes.The end  had come AGAIN.... </p>
+
+        <p>But to their utter surprise,the sun turned fiery yellow again.It finished just like it had started,in a flash.The celebrations started again. It is 
+        rightly said that nothing increases happiness like a surge of gloom. They decided to  have the carnival ,bigger and better.But how did it just go away? 
+        No one cared about it in Fun-o-Land till they got Athelon's message about the heroics of Techiyon and Shutter Island. </p>
+        
+        <p>The grateful Fun-o-Landers decided to shift the carnival to the Ring of Fire and also to include the other islands.They too will travel to witness 
+        the unprecedented ADVAITA...</p>
+			</div> <!-- cd-modal-content -->
+		</div> <!-- cd-modal -->
+
+		<a href="#" class="cd-modal-close">Close</a>
+	   </section>
+        <div class="row">
+            <p><b> Events Coming Soon...</b></p>          
+        </div>
+           <br/><br/>
+	    </div><!-- /container -->
+  </div>
+  </div>
+  <div style="background:#c7d6ff; width:100%; height:5px;"></div>
+  <?php include("../footer.html"); ?>
+
+        
+
+		<script src="../js/classie.js"></script>
+		<script src="../js/gnmenu.js"></script>
+		<script src="../js/velocity.min.js"></script>
+        <script src="../js/main.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  	</body>
+</html>

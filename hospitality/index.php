225410e727916ecaedabf699c2b97e7096147f56
rahsnh <rahsnh@gmail.com>
2017-04-02 23:04:04 +0530

    Add files via upload

---
+++ hospitality/index.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hospitality</title>
+      <link rel="apple-touch-icon" sizes="57x57" href="../website/icon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../website/icon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../website/icon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../website/icon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../website/icon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../website/icon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../website/icon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../website/icon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../website/icon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="../website/icon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="../website/icon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="../website/icon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="../website/icon/favicon-16x16.png">
+<link rel="manifest" href="../website/icon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="../website/icon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+ <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+        <link href="css/head-foot.css" rel="stylesheet" type="text/css">
+		<script src="js/modernizr.custom.js"></script>
+<link href="css/Hover.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<!--Pattern HTML-->
+<div class="content">
+		<ul id="gn-menu" class="gn-menu-main">
+				<li class="gn-trigger">
+					<a class="gn-icon-menu"></a>
+					<span class="gn-icon-menu-2"></span>
+					<span class="gn-icon-menu-3"></span>
+   					 <nav class="gn-menu-wrapper">
+						<div class="gn-scroller">
+							<ul class="gn-menu">
+								<li><a href="../tech" class="gn-icon gn-icon-download">TECHEON<img class="arrow" src="img/arrow.png"></a></li>
+								<li><a href="../cult" class="gn-icon gn-icon-cog">ISLE OF EUPHORIA<img class="arrow" src="img/arrow.png"/></a></li>
+								<li><a "../literary" class="gn-icon gn-icon-help">ISLE OF ERUDITE<img class="arrow" src="img/arrow.png"/></a></li>
+								<li><a href="../ringoffire" class="gn-icon gn-icon-archive">RING OF FIRE<img class="arrow" src="img/arrow.png"/></a></li>
+                                <li><a href="../sports" class="gn-icon gn-icon-archive">ISLE OF ATHLON<img class="arrow" src="img/arrow.png"/></a></li>
+                                <li><a href="../art" class="gn-icon gn-icon-archive">SHUTTER ISLAND<img class="arrow" src="img/arrow.png"/></a></li>
+                                <li><a href="../workshop" class="gn-icon gn-icon-archive">WORKSHOPS<img class="arrow" src="img/arrow.png"/></a></li>
+							</ul>
+						</div><!-- /gn-scroller -->
+					</nav>
+				</li>
+				<li><a href="https://advaita.io/"><img src="img/LOGO WID DATE2.png" /></a></li>
+			</ul>
+       </div>
+  <div id="pattern" class="pattern">
+  	<!--Begin Pattern HTML-->
+		<ul class="navigation" role="navigation">
+			<li><a href="#Hospitality">Hospitality</a></li>
+			<li><a href="#ReachIIIT">Reaching IIIT-Bh</a></li>
+		</ul>
+	</div>
+	<!--End Pattern HTML-->
+	
+	<div class="container">	
+		<section id="Hospitality">
+        <br/><br/><br/>
+			<h1>Hospitality</h1>
+      <p>On behalf of the entire Team Advaita, we would like to offer a warm welcome to you and trust that you treasure each and every minute of your stay at the beautiful campus of IIIT BHUBANESWAR.</p>
+			<h3>Upon reaching IIIT Bhubaneswar-</h3>
+			<ul>
+			<li><p>The hospitality team would be based at the main gate as well every bit deep down the campus</p></li>
+              <li><p>All the participants should report to the accommodation hospi bureau.</p></li>
+              <li><p>The Institute main gate will be open at all times. You may enter and depart from the campus at all times. But please make sure to carry your college ID cards for the purpose of security check at the main gate.</p></li>
+              <li> <p>Alcohol, drugs and explosives of whatever form are strictly forbidden within the campus.</p></li>
+              <li><p>Participants should contact the hospi bureau for queries regarding any event, or any doubts about Advaita in general.</p></li>
+              <li><p>Registration for few events also takes place at the Hospitality bureau.</p></li>
+              <li><p>The Hospitality Desk will be functional from Day 1  (11th Feb) to Day 4 (14th Feb) between 8:00 am to 5:00 pm.</p></li>
+              <li><p>Do visit the Hospitality Desk. We will be Happy to Help.</p><br /></li>
+			</ul>
+			<h3>Accommodation</h3>
+			<ul>
+			<li><p>The participants of the Advaita'16 will be accommodated at the student's hostel in a common room.</p></li>
+              <li><p>Accommodation is provided by registering on our website, those who registered online will be supplied all the provender. Those who did not register on the website can be accommodated by registering at the hospi bureau by means not assuring all the provisions cited.</p></li>
+              <li><p>Students who want to avail the accommodation will have to pay Rs 250/- per day per person.</p></li>
+              <li><p>You will be furnished with a bed, a pillow, a bed sheet, and basic amenities. However, you are encouraged to carry your own blankets since it might get a little cold at night.</p></li>
+              <li><p>Requests for single rooms or any other complaints in this regard will not be entertained.</p></li>
+              <li><p>We are not responsible for your  belongings, check your belongings before you leave.</p></li>
+              <li><p>We take hospitality to be of extreme importance and we make certain that all our participants and guests are taken care of with extreme warmth.</p></li>
+			</ul>
+            <h3>Food and Beverages</h3>
+			<ul>
+			<li><p>In that respect are many kinds of food stalls and coffee stalls providing a unique taste experience as per reminding the taste buds from north and south</p></li>
+              <li><p>There are a variety of canteens inside the campus which provide food at reasonable rates. College is endorsed with a banking facility with 24 hrs service ATM of ANDHRA BANK and also has a shopping complex.</p></li>
+			</ul>
+		</section>
+        <section id="ReachIIIT">
+        <br/><br/><br/>
+        <h1>Reaching IIIT-Bh</h1>
+			<p><strong><a href="https://www.google.co.in/maps/dir/Government+Railway+Police+Station,+Ashok+Nagar,+Bhubaneshwar,+Odisha+751009/IIIT+Bhubaneswar,+Bhubaneshwar,+Odisha/@20.2771293,85.721811,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3a19a74e848e26cd:0x13a124fa5e964de2!2m2!1d85.8431312!2d20.2662926!1m5!1m1!1s0x3a1907923fc9c557:0xd3b0b05336a9dfaa!2m2!1d85.7441969!2d20.295001">From Railway Station to IIIT Bhubaneswar</a></strong></p>
+            <p><strong><a href="https://www.google.co.in/maps/dir/Bhubaneswar+Airport,+Airport+Road,+Aerodrome+Area,+Bhubaneshwar,+Odisha/IIIT+Bhubaneswar,+Bhubaneshwar,+Odisha/@20.274164,85.7150518,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3a19a70f034c758d:0xda434edd567b3553!2m2!1d85.8172289!2d20.2533548!1m5!1m1!1s0x3a1907923fc9c557:0xd3b0b05336a9dfaa!2m2!1d85.7441969!2d20.295001">From Airport to IIIT Bhubaneswar</a></strong></p>
+            <p><strong><a href="https://www.google.co.in/maps/dir/Baramunda+Bus+Stand,+Udayagiribihar,+Baramunda,+Bhubaneshwar,+Odisha/IIIT+Bhubaneswar,+Bhubaneshwar,+Odisha/@20.2771397,85.734832,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3a19a78c1bfaf3a1:0x32aa42e9ecf67d61!2m2!1d85.7921393!2d20.27318!1m5!1m1!1s0x3a1907923fc9c557:0xd3b0b05336a9dfaa!2m2!1d85.7441969!2d20.295001">From Baramunda Bus Station to IIIT Bhubaneswar</a></strong></p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29938.386344633585!2d85.76222134363829!3d20.287916626389343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1907923fc9c557%3A0xd3b0b05336a9dfaa!2sIIIT!5e0!3m2!1sen!2sin!4v1452827596739" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+			
+        </section>
+	</div>
+	<?php include("../footer.html"); ?> 
+
+<script src="js/classie.js"></script>
+		<script src="js/gnmenu.js"></script>
+		<script>
+			new gnMenu( document.getElementById( 'gn-menu' ) );
+		</script>
+
+		   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-72394454-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+	</body>
+</html>
